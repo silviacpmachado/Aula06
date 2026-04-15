405c78c841a496f75cfa4a52b7bf6125ba688742
silviacpmachado-sketch <silviacpmachado@gmail.com>
2026-04-14 21:49:28 -0300

    inicializa

---
+++ aula06/tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+resposta = 'S'
+soma = 0
+
+while resposta != "N":
+    valor = float(input('Digite o valor da compra: '))
+
+    if valor > 1000:
+      desconto = valor * 0.10
+      valor_final = valor - desconto
+    else:
+      valor_final = valor
+
+soma += valor_final
+resposta = input('Quer continuar? [S/N] ').upper().strip()
+
+print(f'O total da soma é: {soma}')
