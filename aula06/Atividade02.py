4f882fa189a56d6771b32bde120ff63405cb0e60
silviacpmachado-sketch <silviacpmachado@gmail.com>
2026-04-16 18:46:49 -0300

    inicializa

--- aula06/Atividade02.py
+++ aula06/Atividade02.py
@@ -1,19 +1,32 @@
-# Valor de venda - 10% para vendas acima de R$ 1.000,00
+# # Valor de venda - 10% para vendas acima de R$ 1.000,00
 
 
+# resposta = 'S'
+# soma = 0
+
+# while resposta != "N":
+#     valor = float(input('Digite o valor da compra: '))
+
+#     if valor > 1000:
+#       desconto = valor * 0.10
+#       valor_final = valor - desconto
+#     else:
+#       valor_final = valor
+
+# soma += valor_final
+# resposta = input('Quer continuar? [S/N] ').upper().strip()
+
+# print(f'O total da soma é: {soma}')
+
+#responda abaixo do professor
 resposta = 'S'
-soma = 0
+while resposta != 'N':
+   valor = float(input('Informe o valor: '))
 
-while resposta != "N":
-    valor = float(input('Digite o valor da compra: '))
+   if valor > 1000:
+      desconto = valor * 0.1
+      valor-= desconto
+      print(f'O valor a pagar é: {valor}')
 
-    if valor > 1000:
-      desconto = valor * 0.10
-      valor_final = valor - desconto
-    else:
-      valor_final = valor
-
-soma += valor_final
-resposta = input('Quer continuar? [S/N] ').upper().strip()
-
-print(f'O total da soma é: {soma}')
+   resposta = input('Quer continuar [S/N]? ').upper()
+print('Programa encerrado')
